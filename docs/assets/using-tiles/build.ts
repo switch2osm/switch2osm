494809593aff8aad940331bf70182425a63e0f9b
Paul Norman <penorman@mac.com>
2025-11-02 18:58:04 -0800

    Fix error in sprite URL

--- docs/assets/using-tiles/build.ts
+++ docs/assets/using-tiles/build.ts
@@ -4,7 +4,7 @@ import { writeFileSync } from "node:fs";
 const style = colorful({
     baseUrl: "https://example.com",
     glyphs: "/fonts/{fontstack}/{range}.pbf",
-    sprite: "/sprites/basics/sprites",
+    sprite: [{ id: 'basics', url: "/sprites/basics/sprites" }],
     tiles: ["https://vector.openstreetmap.org/shortbread_v1/{z}/{x}/{y}.mvt"]
 });
 writeFileSync("release/style.json", JSON.stringify(style));
